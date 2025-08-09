06e27c67c9c62bb8659f1d834e4069b02f179c2b
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 11:54:16 +0100

    Cleans code

--- objcplate/main.m
+++ objcplate/main.m
@@ -1,19 +1,23 @@
 #import <Foundation/Foundation.h>
 
+NSString *getTemplateFile(int argc, const char *argv[]) {
+    if (argc < 2) {
+        @throw @"Invalid args";
+    }
+
+    const char *target = argv[1];
+    NSString *starget = [NSString stringWithUTF8String:target];
+
+    if (starget.length < 3) {
+        @throw @"Invalid target";
+    }
+
+    return [starget substringFromIndex:2];
+}
+
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
-        if (argc < 2) {
-            @throw @"Invalid args";
-        }
-
-        const char *target = argv[1];
-        NSString *starget = [NSString stringWithUTF8String:target];
-
-        if (starget.length < 3) {
-            @throw @"Invalid target";
-        }
-
-        NSString *templateFile = [starget substringFromIndex:2];
+        NSString *templateFile = getTemplateFile(argc, argv);
     }
     return 0;
 }
