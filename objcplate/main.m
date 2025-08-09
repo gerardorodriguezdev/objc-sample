38a76645156e0be480377c2df2c5ace2af8ce9d0
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 11:19:46 +0100

    Checks args

--- objcplate/main.m
+++ objcplate/main.m
@@ -2,13 +2,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        /**
          if (argc < 2) {
              @throw @"Invalid args";
          }
-         */
-        
-        
     }
     return 0;
 }
