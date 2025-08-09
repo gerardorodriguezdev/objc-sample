34a1a584797e4d03ec171242b55b400da379d40f
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 11:52:21 +0100

    Extracts file template

--- objcplate/main.m
+++ objcplate/main.m
@@ -1,10 +1,19 @@
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     @autoreleasepool {
-         if (argc < 2) {
-             @throw @"Invalid args";
-         }
+        if (argc < 2) {
+            @throw @"Invalid args";
+        }
+
+        const char *target = argv[1];
+        NSString *starget = [NSString stringWithUTF8String:target];
+
+        if (starget.length < 3) {
+            @throw @"Invalid target";
+        }
+
+        NSString *templateFile = [starget substringFromIndex:2];
     }
     return 0;
 }
