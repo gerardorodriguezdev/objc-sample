f97486a692c9214aaafecd223f5b62a7f70cd3bb
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 11:16:05 +0100

    Initial Commit

---
+++ objcplate/main.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        /**
+         if (argc < 2) {
+             @throw @"Invalid args";
+         }
+         */
+        
+        
+    }
+    return 0;
+}
